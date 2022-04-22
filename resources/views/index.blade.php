a08b47e6c263809a0f42365253ec62ab3272ed38
iawadancuestamolina <1hiawcuestamolinaadan@gmail.com>
2022-04-22 16:47:29 +0200

    pull

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>All In One Music</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <style>
+            .col-8{
+                background-color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="parallax"></div>
+        <div class="container h-100">
+            <div class="row"></div>
+            <div class="row">
+                <div class="col-4"></div>
+                <div class="col-8">
+                    <h1>AIOM</h1>
+                </div>
+                <div class="col-4"></div>
+            </div>
+            <div class="row"></div>
+        </div>
+
+
+    </body>
+</html>

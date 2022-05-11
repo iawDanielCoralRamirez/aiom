06b75d9b0d669e489694ee2774be505a67adda3f
Dani Coral <danicoral08@gmail.com>
2022-05-11 16:49:43 +0200

    images size

--- resources/views/music_dashboard.blade.php
+++ resources/views/music_dashboard.blade.php
@@ -7,7 +7,7 @@
   <div class="d-flex flex-row justify-content-around flex-wrap">
       @forelse ($songs as $song)
       <div class="card m-2 bg-dark text-white" style="width: 18rem;">
-        <a href="/player/{{$song->id}}"><img class="card-img-top" src="/storage/music/covers/{{$song->cover}}" alt="Card image cap"></a>
+        <a href="/player/{{$song->id}}"><img class="card-img-top cover-size" src="/storage/music/covers/{{$song->cover}}" alt="Card image cap"></a>
         <div class="card-body">
           <h5 class="card-title"><a class="mt-2 text-decoration-none" href="/player/{{$song->id}}">{{$song->title}}</a></h5>
           <div class="container  d-flex">
@@ -40,7 +40,7 @@
   <div class="d-flex flex-row justify-content-around flex-wrap">
       @forelse ($favorites_songs as $favorite)
       <div class="card m-2 bg-dark text-white" style="width: 18rem;">
-        <a href="/player/{{$favorite->id}}"><img class="card-img-top" src="/storage/music/covers/{{$favorite->cover}}" alt="Card image cap"></a>
+        <a href="/player/{{$favorite->id}}"><img class="card-img-top cover-size" src="/storage/music/covers/{{$favorite->cover}}" alt="Card image cap"></a>
         <div class="card-body">
           <h5 class="card-title"><a class="mt-2 text-decoration-none text-white" href="/player/{{$favorite->id}}">{{$favorite->title}}</a></h5>
           <div class="container  d-flex">
@@ -73,7 +73,7 @@
   <div class="d-flex flex-row justify-content-around flex-wrap">
       @forelse ($recently_songs as $song)
       <div class="card m-2 bg-dark text-white" style="width: 18rem;">
-        <a class="text-decoration-none text-dark" href="/player/{{$song->id}}"><img class="card-img-top" src="/storage/music/covers/{{$song->cover}}" alt="Card image cap"></a>
+        <a class="text-decoration-none text-dark" href="/player/{{$song->id}}"><img class="card-img-top cover-size" src="/storage/music/covers/{{$song->cover}}" alt="Card image cap"></a>
         <div class="card-body">
           <h5 class="card-title"><a class="mt-2 text-decoration-none text-white" href="/player/{{$song->id}}">{{$song->title}}</a></h5>
           <div class="container  d-flex">
@@ -107,9 +107,9 @@
     @forelse ($playlists as $playlist)
       <div class="card bg-dark text-white m-2" style="width: 18rem;">
         @if ($playlist->cover != null)
-          <img class="card-img-top" src="storage/playlist/covers/{{ $playlist->cover }}" alt="Card image cap">
+          <img class="card-img-top cover-size" src="storage/playlist/covers/{{ $playlist->cover }}" alt="Card image cap">
         @else 
-          <img class="card-img-top" src="img/default-playlist.jpeg" alt="Card image cap">
+          <img class="card-img-top cover-size" src="img/default-playlist.jpeg" alt="Card image cap">
         @endif
         <div class="card-body">
           <h5 class="card-title">{{$playlist->name}}</h5>
